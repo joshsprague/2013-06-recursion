476c2f38b14e6fe452111050626491a6e119edd8
joshsprague <jjspragu@asu.edu>
2013-06-05 17:40:52 -0700

    It loops through and puts pairs in right place, but missing quotes

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,26 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
+  var results = [];
+  var pairs = [];
+  //need to take a key and a value and put quotes around them, returning as an object of key/value pairs;
+
+  //how to get obj key:
+  //obj[1] = value of first pair
+  //obj["key"] = "value";
+  //make the key variable equal a string
+  //probably have to do this as a regExp like the last one
+
+  for (key in obj) {
+  	pairs.push('"' + key + ":" + obj[key]);
+  }
+
+  //can join pairs with .join(',') then concat the curly braces
+  pairs = pairs.join(',');
+  results = results.concat('{' , pairs , '}');
+
+  //var key = new RegExp("(^|\\s)" + obj[1] + "(\\s|$)");
+
+  
+  return obj;
 };
